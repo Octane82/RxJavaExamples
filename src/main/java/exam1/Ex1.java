8110b9c3830f066c841bebc64af1da13577d1ac5
Octane_cadcam <octanek@hotmail.com>
2016-10-07 20:55:31 +0300

    - Add Ex1 and Ex2

---
+++ src/main/java/exam1/Ex1.java
@@ -0,0 +1,89 @@
+package exam1;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+
+
+/**
+ * Created
+ */
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+        new Ex1().sample1();
+
+
+
+    }
+
+
+    private void sample1() {
+
+        // Способ 1
+            // myObservable.subscribe(mySubscriber);
+
+        // Порождает один элемент данных, а потом завершает своё выполнение
+        Observable<String> myObserv2 = Observable.just("Hello world. YAY");
+
+        //myObserv2.subscribe(mySubscriber);
+
+        // ******************
+            // myObservable.subscribe(onNextActions);      // myObservable.subscribe(onNextAction, onErrorAction, onCompleteAction);
+
+
+        // Сокращённая запись (с лямбдой)
+        Observable.just("Hello sample").subscribe(s -> {
+            System.out.println(s);
+        });
+
+    }
+
+
+    /**
+     * Излучает данные
+     */
+    Observable<String> myObservable = Observable.create(new Observable.OnSubscribe<String>() {
+        @Override
+        public void call(Subscriber<? super String> subscriber) {
+            subscriber.onNext("Hello world!");
+            subscriber.onCompleted();
+        }
+    });
+
+
+    /**
+     * Ловит данные (ПОДПИСЧИК)
+     */
+    Subscriber<String> mySubscriber = new Subscriber<String>() {
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+
+        }
+
+        @Override
+        public void onNext(String s) {
+            System.out.println(s);
+        }
+    };
+
+
+    /**
+     * 2 - й выпиант subscriber с вызывом одного метода
+     */
+   Action1<String> onNextActions = new Action1<String>() {
+       @Override
+       public void call(String s) {
+           System.out.println(s);
+       }
+   };
+
+
+
+}
---
+++ src/main/java/exam1/Ex2.java
@@ -0,0 +1,62 @@
+package exam1;
+
+
+import rx.Observable;
+import rx.functions.Func1;
+
+/**
+ *  Операторы могут быть использованы в промежутке
+ *  между Observable и Subscriber для манипуляции данными
+ */
+public class Ex2 {
+
+    public static void main(String[] args) {
+
+        new Ex2().sample1();
+
+    }
+
+
+    /**
+     * RUN
+     */
+    private void sample1() {
+
+        Observable.just("Hello world")
+                .map(new Func1<String, String>() {                  // через оператор .map можно преобразовывать один элемент данных в другой: (в разрыв Observable и Subscriber)
+                    @Override
+                    public String call(String s) {
+                        return s + " -Dima";
+                    }
+                })
+                .subscribe(s -> System.out.println(s));
+
+
+        // Также можем изменять на выход значение другого типа
+        Observable.just("Hello HASH!")
+                .map(new Func1<String, Integer>() {
+                    @Override
+                    public Integer call(String s) {
+                        return s.hashCode();
+                    }
+                })
+                .subscribe(i -> System.out.println(Integer.toString(i)));
+
+
+        //
+        Observable.just("Hello, world!")
+                .map(s -> s.hashCode())
+                .map(i -> Integer.toString(i))
+                .subscribe(s -> System.out.println(s));
+
+
+    }
+
+
+    /**
+     * https://habrahabr.ru/post/265583/ - GROKAEM 2
+     */
+
+
+}
+
